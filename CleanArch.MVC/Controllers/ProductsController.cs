8a0c6e7e5062df27e12bc7c5d2a0cc729bd13c72
Marcos Vinicius <marcos.viniciusk19@gmail.com>
2024-08-09 23:07:51 -0300

    .NET - Implementando a Clean Architecture - V

---
+++ CleanArch.MVC/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        
+        public async Task<IActionResult> Index()
+        {
+            var result = await _productService.GetProducts();
+            return View(result);
+        }
+
+        [HttpGet()]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create([Bind("Id, Name, Description, Price")] ProductViewModel product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.Add(product);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+    }
+}
